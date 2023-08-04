f068eb034b1f7a86deab448f55b95a004f2fa00b
mohammed <mohammedhanda109@gmail.com>
2023-08-04 01:21:44 +0100

    add person component and use props to pass values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last name: Smith</h2>
+      <h3>Age: {props.age}</h3>
+    </>
+  )
+}
+
 const App = () => {
+  const name = "Jhon";
+  const age =23
   return (
     <div className="App">
-    <h1>Hello, Change</h1>
+    <h1>{ "Hello" }</h1>
+    {name ? 
+      (<>
+          <Person name={name} age={age} />
+      </>):
+      (<>
+        <h2>No name provided</h2>
+      </>)}
     </div>
   );
 }
