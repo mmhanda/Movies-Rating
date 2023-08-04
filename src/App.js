d75bdfebb1f4d8825293dde4d92bbb9f45ef3f21
mohammed <mohammedhanda109@gmail.com>
2023-08-04 01:58:02 +0100

    make small usestate counter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
-
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last name: Smith</h2>
-      <h3>Age: {props.age}</h3>
-    </>
-  )
-}
+import {useState} from 'react';
 
 const App = () => {
-  const name = "Jhon";
-  const age =23
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-    <h1>{ "Hello" }</h1>
-    {name ? 
-      (<>
-          <Person name={name} age={age} />
-      </>):
-      (<>
-        <h2>No name provided</h2>
-      </>)}
+    <button onClick={() => {setCounter((prevcount) => prevcount - 1)}}>-</button>
+      <h1>{counter}</h1>
+    <button onClick={() => {setCounter((prevcount) => prevcount + 1)}}>+</button>
     </div>
   );
 }
