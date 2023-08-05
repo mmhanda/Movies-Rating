e227512fa49b3b0f09d83ac9f8b007794ad1b76a
mohammed <mohammedhanda109@gmail.com>
2023-08-05 06:03:01 +0100

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ const API_URL = "http://www.omdbapi.com?apikey=565ea02c";
 const App = () => {
   
   const [movies, setMovies] = useState([]);
-  const [searchPhrase, setSearchPhrase] = useState('');
+  const [searchPhrase, setSearchPhrase] = useState("");
 
+  
   const searchMovie = async(title) => {
-    const respones = await fetch(`${API_URL}&t=${title}`);
+    const respones = await fetch(`${API_URL}&s=${title}`);
     const data = await respones.json();
+    console.log(data.Search);
     setMovies(data.Search);
   }
 
@@ -32,28 +34,29 @@ const App = () => {
         />
         <img
           src={SearchIcon}
+          alt="search"
           onClick={() => {
             searchMovie(searchPhrase);
           }}
         />
       </div>
 
-      { movies?.length > 0
+      {movies?.length > 0
         ? (
             <div className="container">
-            {movies.map((movie) => {
+            {movies.map((movie) => (
               <MovieCard movie={movie}/>
-            })}
+        ))}
             </div>
         ) : (
             <div className="empty">
               <h2>Sorry No Movie Found</h2>
             </div>
-          )
+        )
       }
 
     </div>
   );
 }
 
-export default App
+export default App;
